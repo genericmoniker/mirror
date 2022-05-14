581cf2be8f9adbbea485c9cc9ff5d6291e7a4111
Eric Smith <eric@esmithy.net>
2022-05-14 14:13:07 -0600

    Add goal reached indication

--- frontend/src/Activity.svelte
+++ frontend/src/Activity.svelte
@@ -3,19 +3,21 @@
   import { onMount } from "svelte";
   import Icon from "svelte-awesome";
   import { faShoePrints } from "@fortawesome/free-solid-svg-icons";
+  import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
   export let width;
   let height = 11;
 
   let canvas;
   let stepsStr = "0";
+  let percent = 0;
 
   subscribe("activity.refresh", (e) => {
     let data = JSON.parse(e.data);
     let steps = data.steps;
     stepsStr = steps.toLocaleString();
     let stepsGoal = data.stepsGoal;
-    let percent = steps / stepsGoal;
+    percent = steps / stepsGoal;
     let barWidth = Math.min(1, percent) * width;
     drawProgressBar(barWidth);
   });
@@ -46,9 +48,15 @@
 
 <div id="component">
   <div id="text" style="width: {width}px">
-    <Icon id="icon" data={faShoePrints} scale="2" />
+    <Icon id="steps-icon" data={faShoePrints} scale="2" />
     &nbsp;
     {stepsStr}
+    {#if percent >= 1}
+      &nbsp;
+      <span id="goal-icon"
+        ><Icon id="goal-icon" data={faCheckCircle} scale="1.6" /></span
+      >
+    {/if}
   </div>
   <canvas bind:this={canvas} {width} {height} />
 </div>
@@ -60,5 +68,18 @@
   #text {
     text-align: center;
     vertical-align: middle;
+  }
+  #goal-icon {
+    opacity: 1;
+    animation: fade 8s ease-in-out infinite;
+  }
+  @keyframes fade {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
   }
 </style>
