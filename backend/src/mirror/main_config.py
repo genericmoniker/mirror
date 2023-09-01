d1b570d0ecbd521d5a73d1d5d4358c3a1e3f3c10
Eric Smith <eric@esmithy.net>
2023-08-31 20:29:46 -0600

    Show available plugins in configure help

--- backend/src/mirror/main_config.py
+++ backend/src/mirror/main_config.py
@@ -5,13 +5,16 @@ from mirror.plugin_discovery import discover_plugins
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Mirror configuration utility")
+    plugins = discover_plugins()
+    parser = argparse.ArgumentParser(
+        description="Mirror configuration utility",
+        epilog="Available plugins: " + ", ".join(plugins.keys()),
+    )
     parser.add_argument("--plugins", nargs="*")
     args = parser.parse_args()
 
     # TODO: Set up file logging (since we're using stdout).
 
-    plugins = discover_plugins()
     plugins_to_configure = args.plugins or plugins.keys()
 
     for name in plugins_to_configure:
