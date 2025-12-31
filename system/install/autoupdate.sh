aeb3da4db64c496ef3db9e95f4bdb2a672a8dc73
Eric Smith <eric@esmithy.net>
2025-12-30 20:10:31 -0700

    Update autoupdate

---
+++ system/install/autoupdate.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Check for updates by comparing the current git hash with the latest git hash
+# on the main branch. If there is a difference, update the system.
+
+set -euo pipefail
+
+# Log all output
+exec 1> >(logger --tag called-update --priority user.info)
+exec 2> >(logger --tag called-update --priority user.error)
+
+echo "Starting update check..."
+
+# Run from the deployment/server directory
+cd "$(dirname "$0")/.."
+
+current_hash=$(git rev-parse HEAD)
+latest_hash=$(git ls-remote origin -h refs/heads/main | cut -f1)
+
+if [ "$current_hash" != "$latest_hash" ]; then
+    echo "Application update available. Updating to commit hash ${latest_hash}..."
+    git pull || { echo "Failed to pull latest changes"; exit 1; }
+    ./update-app-user.sh || { echo "Failed to run update script"; exit 1; }
+    echo "Application updated."
+else
+    echo "Application is up to date at commit hash ${current_hash}."
+fi
