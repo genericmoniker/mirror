0d82fc5eb1aae04155cf0838745b708fd11aa7c4
Eric Smith <eric@esmithy.net>
2025-12-30 20:49:16 -0700

    More

--- system/install/autoupdate.sh
+++ system/install/autoupdate.sh
@@ -11,7 +11,7 @@ exec 2> >(logger --tag called-update --priority user.error)
 
 echo "Starting update check..."
 
-# Run from the deployment/server directory
+# Run from this script's parent directory
 cd "$(dirname "$0")/.."
 
 current_hash=$(git rev-parse HEAD)
@@ -20,7 +20,7 @@ latest_hash=$(git ls-remote origin -h refs/heads/main | cut -f1)
 if [ "$current_hash" != "$latest_hash" ]; then
     echo "Application update available. Updating to commit hash ${latest_hash}..."
     git pull || { echo "Failed to pull latest changes"; exit 1; }
-    ../install.sh || { echo "Failed to run install script"; exit 1; }
+    ./install.sh || { echo "Failed to run install script"; exit 1; }
     echo "Application updated."
 else
     echo "Application is up to date at commit hash ${current_hash}."
