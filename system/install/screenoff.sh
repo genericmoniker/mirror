c58d7c64da22a8ca446d1a81dcee22a20025c260
Eric Smith <eric@esmithy.net>
2026-04-19 07:28:58 -0600

    Use wlopm for screen on/off (Wayland)

--- system/install/screenoff.sh
+++ system/install/screenoff.sh
@@ -2,8 +2,5 @@
 
 echo "Turning screen off"
 
-# Turn dpms on (so we can use it to turn the screen off)
-xset +dpms -display :0
-
-# Turn the screen off
-xset dpms force off -display :0
+# Turn the screen off using wlopm (Wayland)
+wlopm --off '*'
--- system/install/screenon.sh
+++ system/install/screenon.sh
@@ -2,11 +2,5 @@
 
 echo "Turning screen on"
 
-# Turn dpms on (so we can use it, just in case it is off)
-xset +dpms -display :0
-
-# Turn the screen on
-xset dpms force on -display :0
-
-# Turn dpms off so that the screen will not turn off after the timeout
-xset -dpms -display :0
+# Turn the screen on using wlopm (Wayland)
+wlopm --on '*'
